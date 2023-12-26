52411c8189b82a9d8702ae3e7ce3ad6e9e312913
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2023-12-26 11:10:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your JS code here. If required.
+const form = document.querySelector('form');
+const userAge = form.age;
+const userName = form.name;
+
+form.addEventListener('submit', (event) => {
+
+    event.preventDefault();
+
+    // validation
+    if (userName.value === '' || userAge.value === '') {
+        alert('Please fill both fields');
+    }
+
+    // Input verification
+    const verfiyAgePromise = new Promise(function executor(resolve, reject) {
+
+        const age = Number(userAge.value);
+        console.log('checking user age');
+
+        // above 18, valid
+        if (age >= 18) {
+            setTimeout(() => {
+                resolve(`Welcome, ${userName.value}. You can vote`);
+            }, 4000);
+        }
+        else {
+            setTimeout(() => {
+                reject(`Oh sorry ${userName.value}. You aren't old enough`);
+            })
+        }
+    });
+
+    verfiyAgePromise
+    .then((message)=> {
+        alert(message);
+    })
+    .catch((message)=> {
+        alert(message);
+    })
+})
